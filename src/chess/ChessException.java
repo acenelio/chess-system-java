8f81088a6507b4e1a35b7302004b1cbb018d838b
Nelio Alves <acenelio@gmail.com>
2018-06-15 17:11:29 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,9 @@
+package chess;
+
+public class ChessException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ChessException(String msg) {
+		super(msg);
+	}
+}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -24,9 +23,13 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
 	}
 }
