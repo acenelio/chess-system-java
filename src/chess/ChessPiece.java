0b1fcdbb825886047e9dd7e6b7ec46e7a11730ee
Nelio Alves <acenelio@gmail.com>
2021-09-01 07:21:44 -0300

    Fix: protected increaseMoveCount decreaseMoveCount

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -22,11 +22,11 @@ public abstract class ChessPiece extends Piece {
 		return moveCount;
 	}
 	
-	public void increaseMoveCount() {
+	protected void increaseMoveCount() {
 		moveCount++;
 	}
 
-	public void decreaseMoveCount() {
+	protected void decreaseMoveCount() {
 		moveCount--;
 	}
 
